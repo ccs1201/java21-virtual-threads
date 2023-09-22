26e4a9f73091fd1586847f78bcb05573c1315df3
Cleber Souza <ccs1201@gmail.com>
2023-09-22 01:13:28 -0300

    #3

--- src/main/run.bat
+++ src/main/run.bat
@@ -1,17 +1,17 @@
 del *.log
 del *json
 k6 run --summary-export custom.json .\resources\test_scripts\custom.js
-timeout /t 3 /nobreak
-k6 run .\resources\test_scripts\total.js
 timeout /t 10 /nobreak
+k6 run .\resources\test_scripts\total.js
+timeout /t 3 /nobreak
 k6 run --summary-export platform.json .\resources\test_scripts\forkJoin.js
-timeout /t 3 /nobreak
-k6 run .\resources\test_scripts\total.js
 timeout /t 10 /nobreak
+k6 run .\resources\test_scripts\total.js
+timeout /t 3 /nobreak
 k6 run --summary-export virtual.json .\resources\test_scripts\virtual.js
-timeout /t 3 /nobreak
-k6 run .\resources\test_scripts\total.js
 timeout /t 10 /nobreak
-k6 run --summary-export all.json .\resources\test_scripts\simultaneo.js
+k6 run .\resources\test_scripts\total.js
 timeout /t 3 /nobreak
+k6 run --summary-export all.json .\resources\test_scripts\simultaneo.js
+timeout /t 10 /nobreak
 k6 run .\resources\test_scripts\total.js
